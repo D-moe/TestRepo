0c458e448602505fa7a86988be668d04074fa663
morleyd <morleyd@penguin>
2023-05-01 22:19:07 -0700

    Resolve bugs, add pymakr.json

--- install.sh
+++ install.sh
@@ -4,32 +4,38 @@
 check='\xE2\x9C\x94'
 cross='\xE2\x9D\x8C'
 RED='\033[0;31m'
-GREEN='0;32'
+GREEN='\033[0;32m'
 NC='\033[0m'
 pymakr_config_path='~/.config/Code/User'
 pymakr_config_url=''
 
 # Check if the given install was successful
-function did_succeed()
+function did_succeed() {
     # Check if the file is empty
-    if [ -s error e]; then
-        echo -e "${RED}${cross}An error occurred${NC}"
+    if [ -s error ]; then
+        echo -e "${RED}${cross}An error occurred $1${NC}"
         cat error
         exit -1
     else
-        xecho -e "${GREEN}${check}Successfully installed $1${NC}"
+        echo -e "${GREEN}${check}Successfully ran $1${NC}"
+    fi
+}
 
 
-sudo apt update && sudo apt upgrade -y > /dev/null 2>error
+sudo apt-get update && sudo apt-get upgrade -y 2>error
 did_succeed "Updating Packages"
 code_deb="code_1.77.3-1681292746_amd64.deb"
-wget -O $code_deb https://go.microsoft.com/fwlink/?LinkID=760868 > /dev/null 2>error
+wget -O $code_deb https://go.microsoft.com/fwlink/?LinkID=760868 2>deb_status
+# Technically not a real check here, TODO(morleyd): Fix to proper check
 did_succeed "Fetching Code File "
-sudo apt install ./$code_deb > /dev/null 2>error
+sudo apt-get install ./$code_deb -y 2>error
 did_succeed "Installing Code"
-sudo apt install nodejs > /dev/null 2>error
+sudo apt-get install nodejs 2>error
 did_succeed "Installing NodeJs"
-code --install-extension pycom.pymakr@1.1.18 > /dev/null 2>error
+code --install-extension pycom.pymakr@1.1.18 2>error
 did_succeed "Installing PyMakr Extension"
-wget -O ${pymakr_config_path}/pymakr.json ${pymakr_config_url} > /dev/null 2>error
+wget -O ${pymakr_config_path}/pymakr.json ${pymakr_config_url} 2>error
 did_succeed "Copy PyMakr Config"
+
+# If we get here, than everything successfully ran, so remove our tmp files.
+rm deb_status error
