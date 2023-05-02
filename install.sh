dbb505aad5e72f0c3a5f74eb1c9f4b7a52d29bda
morleyd <morleyd@penguin>
2023-05-01 22:56:20 -0700

    Minor tweaks

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 pymakr_config_path='~/.config/Code/User'
-pymakr_config_url=''
+pymakr_config_url='https://raw.githubusercontent.com/D-moe/TestRepo/main/pymakr.json'
 
 # Check if the given install was successful
 function did_succeed() {
@@ -20,21 +20,25 @@ function did_succeed() {
         echo -e "${GREEN}${check}Successfully ran $1${NC}"
     fi
 }
-
-
-sudo apt-get update && sudo apt-get upgrade -y 2>error
+echo "" > out
+echo "Starting Updating Packages, this may take a while"
+sudo apt-get update > out && sudo apt-get upgrade -y >> out 2>error
 did_succeed "Updating Packages"
 code_deb="code_1.77.3-1681292746_amd64.deb"
-wget -O $code_deb https://go.microsoft.com/fwlink/?LinkID=760868 2>deb_status
+wget -O $code_deb https://go.microsoft.com/fwlink/?LinkID=760868 >> out 2>deb_status
 # Technically not a real check here, TODO(morleyd): Fix to proper check
 did_succeed "Fetching Code File "
-sudo apt-get install ./$code_deb -y 2>error
+sudo apt-get install ./$code_deb -y >> out 2>error
 did_succeed "Installing Code"
-sudo apt-get install nodejs 2>error
+sudo apt-get install nodejs >> out 2>error
 did_succeed "Installing NodeJs"
-code --install-extension pycom.pymakr@1.1.18 2>error
+# Hide warning about Buffer() being deprecated
+export NODE_OPTIONS="--no-deprecation"
+code --install-extension pycom.pymakr@1.1.18 >> out 2>error
 did_succeed "Installing PyMakr Extension"
-wget -O ${pymakr_config_path}/pymakr.json ${pymakr_config_url} 2>error
+wget -O pymakr.json ${pymakr_config_url} >> out 2>error
+eval pymakr_file_name=${pymakr_config_path}/pymakr.json
+cp pymakr.json ${pymakr_file_name} >> out 2>error
 did_succeed "Copy PyMakr Config"
 
 # If we get here, than everything successfully ran, so remove our tmp files.
